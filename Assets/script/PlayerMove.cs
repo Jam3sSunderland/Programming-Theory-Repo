8e8f59eac7b0a792e947a1000cb1e110445aac35
Jam3sSunderland <smithnick1989@gmail.com>
2024-09-09 16:31:06 -0500

    got players and emenys
    got player kindof working

---
+++ Assets/script/PlayerMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float horsePower = 0;
+    private float turnSpeed = 45.0f;
+    public float forWardInput;
+    public float horizontalInput;
+    public float speed;
+    private Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forWardInput = Input.GetAxis("Vertical");
+        // move car
+        //  transform.Translate(Vector3.forward *Time.deltaTime*speed * forWardInput);
+        playerRB.AddRelativeForce(Vector3.forward * horsePower * forWardInput);
+        // rotate car 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        speed = Mathf.RoundToInt(playerRB.velocity.magnitude * 2.237f);
+    }
+}
