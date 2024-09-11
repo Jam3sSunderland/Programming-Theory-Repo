0e266b32914a17dfbcc9780b8222e1d6f0b6f8a1
Jam3sSunderland <smithnick1989@gmail.com>
2024-09-11 12:21:58 -0500

    added a better camera
    new camera script

---
+++ Assets/script/Camera follow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public Transform camTarget;
+    public float pLerp = 0.2f;
+    public float rLerp = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, camTarget.position, pLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.rotation, rLerp);
+    }
+}
