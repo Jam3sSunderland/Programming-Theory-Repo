4fd16a5603dc74336e0596d35c141b956db7e398
Jam3sSunderland <smithnick1989@gmail.com>
2024-09-13 16:31:50 -0500

    added fireball to wizerd
    it shots

---
+++ Assets/script/Fire ball.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : Enemy
+{
+    private float moveRate = 2;
+    private float moverTimer;
+
+    protected override void FollowThePlayer()
+    {
+        base.FollowThePlayer();
+        RandomMover();
+    }
+
+    private void RandomMover()
+    {
+        moverTimer += Time.deltaTime;
+
+        if (moverTimer > moveRate)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
