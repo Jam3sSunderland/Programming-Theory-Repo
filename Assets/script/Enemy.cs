074615ae8ff2bd8f6bca52d8817d702e2126b6cd
Jam3sSunderland <smithnick1989@gmail.com>
2024-09-15 14:33:20 -0500

    add more to enemy script and spider
    jumping spider can go to waypoints and

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 2;
+    [SerializeField] private string enemyName;
+   [SerializeField] protected private float speed = 2;
+    private float healthPoint;
+    [SerializeField] private float maxHealthPoint;
+
     protected private Rigidbody enemyRb;
     public GameObject player;
     public float deSpawn;
+    public float distance;
+    
     // Start is called before the first frame update
     void Start()
     {
+       
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player1");
+        healthPoint = maxHealthPoint;
+        Introduction();
+    }
+
+    private void Introduction()
+    {
+        Debug.Log("My Name is: " + enemyName + "HP:" + healthPoint + "moveSpeed:" + speed);
     }
 
     // Update is called once per frame
@@ -24,9 +38,13 @@ public class Enemy : MonoBehaviour
     }
      protected virtual void FollowThePlayer()
     {
-        Vector3 looky = (player.transform.position - transform.position).normalized;
+        if (Vector3.Distance(transform.position, player.transform.position) < distance)
+        {
+            Vector3 looky = (player.transform.position - transform.position).normalized;
 
-        enemyRb.AddForce(looky * speed);
+            enemyRb.AddForce(looky * speed);
+        }
+       
     }
     public void DespawnEnemy()
     {
--- Assets/script/Jumping spider.cs
+++ Assets/script/Jumping spider.cs
@@ -7,13 +7,37 @@ public class Jumpingspider : Enemy
     public float jumpForce;
     public float jumpTimer;
     public float jumpRate = 2f;
+    public Transform wayPoint01, wayPoint02;
+    private Transform wayPointTarget;
+
+    private void Awake()
+    {
+        wayPointTarget = wayPoint01;
+    }
 
 
+    protected override void FollowThePlayer()
+    {
+        base.FollowThePlayer();
+        if (Vector3.Distance(transform.position, player.transform.position) > distance)
+        {
+            if (Vector3.Distance(transform.position, wayPoint01.position) < 1f)
+            {
+                wayPointTarget = wayPoint02;
+            }
+            if ((Vector3.Distance(transform.position, wayPoint02.position) < 1f))
+            {
+                wayPointTarget = wayPoint01;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, wayPointTarget.position, speed * Time.deltaTime);
+
+        }
+    }
     private void Update()
     {
-        jumpTime();
+        JumpTime();
     }
-    private void jumpTime()
+    private void JumpTime()
     {
         jumpTimer += Time.deltaTime;
 
